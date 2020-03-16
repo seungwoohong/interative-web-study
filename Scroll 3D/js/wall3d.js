eecc5dca8015486bda11cdbf3cd7ba1d364af8f7
seungwoohong <alwls601@naver.com>
2020-03-16 23:55:33 +0900

    :hammer: Scroll 구현

--- Scroll 3D/js/wall3d.js
+++ Scroll 3D/js/wall3d.js
@@ -1,7 +1,32 @@
 (function () {
-    const houseElem = document.querySelector('.house');
+    const $house = document.querySelector('.house');
+    const $progressbar = document.querySelector('.progress-bar');
+    const $stage =  document.querySelector('.stage');
+    const mousePos = { x: 0, y: 0 };
 
-    window.addEventListener('scroll', function() {
-        console.log(this.pageYOffset)
-    });
+    let maxScrollValue = null;
+
+    function resizeHandler() {
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
+
+    function onScroll() {
+        const scrollPer = pageYOffset / maxScrollValue;
+        const zMove = scrollPer * 980 - 490;
+        $house.style.transform = 'translateZ(' + zMove + 'vw)';
+        $progressbar.style.width = scrollPer * 100 + '%';
+    }
+
+    function onMouseMove(e) {
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+        mousePos.y = 1 + (e.clientY / window.innerHeight) * 2;
+
+        $stage.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)';
+    }
+
+    window.addEventListener('scroll', onScroll);
+    window.addEventListener('resize', resizeHandler);
+    window.addEventListener('mousemove', onMouseMove);
+
+    resizeHandler();
 })();
